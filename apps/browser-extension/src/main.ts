1de20635ea48445d738df726521ce3126bce3d4f
Simon Bumiller <simon@bumiller.me>
2025-10-16 16:53:52 +0200

    Revert "Fixed sonar issue"
    This reverts commit 899f0ad41c2638c080510d30175574effc3c9940.

--- apps/browser-extension/src/main.ts
+++ apps/browser-extension/src/main.ts
@@ -2,8 +2,4 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-try {
-  await bootstrapApplication(App, appConfig);
-} catch (err) {
-  console.error(err);
-}
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
