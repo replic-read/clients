dc7b9797c8c54c4bdbef5aa69a7ce82f2aa1739a
Simon Bumiller <simon@bumiller.me>
2025-10-17 12:41:04 +0200

    Fixed exporting

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -2,12 +2,12 @@ export * from './lib/shared/network-client/client';
 export * from './lib/shared/network-client/BaseUrlSupplier';
 export * from './lib/shared/network-client/requests';
 export * from './lib/shared/network-client/responses';
+export * from './lib/shared/network-client/AuthTokenAccessor';
 
 export * from './lib/shared/model/models';
 export * from './lib/shared/model/error';
 export * from './lib/shared/model/loading';
 export * from './lib/shared/model/enums';
 
-export * from './lib/shared/authentication/AuthenticationService';
 export * from './lib/shared/authentication/AuthenticationService';
 export * from './lib/shared/authentication/errors';
