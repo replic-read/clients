81480b9f5a0990dd51a49fce328bb79c306469c9
Simon Bumiller <simon@bumiller.me>
2025-10-17 12:34:42 +0200

    Exported new interfaces from shared

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,5 +1,13 @@
 export * from './lib/shared/network-client/client';
 export * from './lib/shared/network-client/BaseUrlSupplier';
-export * from './lib/shared/network-client/enums';
 export * from './lib/shared/network-client/requests';
 export * from './lib/shared/network-client/responses';
+
+export * from './lib/shared/model/models';
+export * from './lib/shared/model/error';
+export * from './lib/shared/model/loading';
+export * from './lib/shared/model/enums';
+
+export * from './lib/shared/authentication/AuthenticationService';
+export * from './lib/shared/authentication/AuthenticationService';
+export * from './lib/shared/authentication/errors';
