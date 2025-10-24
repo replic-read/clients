fa3d785c4fc4f1e39819d477530a51e8b8f6e8b7
Simon Bumiller <simon@bumiller.me>
2025-10-24 23:12:24 +0200

    renamed login filename

--- apps/browser-extension/src/navigation/routes.ts
+++ apps/browser-extension/src/navigation/routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { SetEndpointViewModel } from '../components/set_endpoint/set_endpoint';
 import { SignupViewModel } from '../components/signup/SignupViewModel';
-import { LoginViewModel } from '../components/login/LoginViewModel';
+import { LoginViewModel } from '../components/login/login';
 import { NotVerifiedViewModel } from '../components/not_verified/NotVerifiedViewModel';
 import { CreateReplicViewModel } from '../components/create_replic/CreateReplicViewModel';
 import { ReplicCreatedViewModel } from '../components/replic_created/ReplicCreatedViewModel';
