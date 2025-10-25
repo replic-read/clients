7ef05586c95c3a99da7cb021d1c6ae965abb1843
Simon Bumiller <simon@bumiller.me>
2025-10-25 22:53:47 +0200

    Rename file of not verified viewmodel

--- apps/browser-extension/src/navigation/routes.ts
+++ apps/browser-extension/src/navigation/routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { SetEndpointViewModel } from '../components/set_endpoint/set_endpoint';
 import { SignupViewModel } from '../components/signup/signup';
 import { LoginViewModel } from '../components/login/login';
-import { NotVerifiedViewModel } from '../components/not_verified/NotVerifiedViewModel';
+import { NotVerifiedViewModel } from '../components/not_verified/not_verified';
 import { CreateReplicViewModel } from '../components/create_replic/create_replic';
 import { ReplicCreatedViewModel } from '../components/replic_created/replic_created';
 import { HomeViewModel } from '../components/home/home';
