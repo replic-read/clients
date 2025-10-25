946cd2bdc7dd9dd84e29d7ca3cac598cd2371280
Simon Bumiller <simon@bumiller.me>
2025-10-25 18:41:27 +0200

    Renamed file for replic created

--- apps/browser-extension/src/navigation/routes.ts
+++ apps/browser-extension/src/navigation/routes.ts
@@ -4,7 +4,7 @@ import { SignupViewModel } from '../components/signup/signup';
 import { LoginViewModel } from '../components/login/login';
 import { NotVerifiedViewModel } from '../components/not_verified/NotVerifiedViewModel';
 import { CreateReplicViewModel } from '../components/create_replic/create_replic';
-import { ReplicCreatedViewModel } from '../components/replic_created/ReplicCreatedViewModel';
+import { ReplicCreatedViewModel } from '../components/replic_created/replic_created';
 import { HomeViewModel } from '../components/home/home';
 
 /**
